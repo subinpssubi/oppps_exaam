1715f549dabb1521be73746cd41b0ababe9e977c
subinpssubi <subinpssubi4@gmail.com>
2025-01-29 11:12:29 +0530

    first commit

---
+++ bin/oppps_exaam.dart
@@ -0,0 +1,5 @@
+import 'package:oppps_exaam/oppps_exaam.dart' as oppps_exaam;
+
+void main(List<String> arguments) {
+  print('Hello world: ${oppps_exaam.calculate()}!');
+}
---
+++ test/oppps_exaam_test.dart
@@ -0,0 +1,8 @@
+import 'package:oppps_exaam/oppps_exaam.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
