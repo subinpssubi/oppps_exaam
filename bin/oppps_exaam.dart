6baf46bb938e6ae629a60c682a7619a33ec28811
subinpssubi <subinpssubi4@gmail.com>
2025-01-29 12:51:21 +0530

    ssss

--- bin/oppps_exaam.dart
+++ bin/oppps_exaam.dart
@@ -1,5 +1,15 @@
 import 'package:oppps_exaam/oppps_exaam.dart' as oppps_exaam;
 
-void main(List<String> arguments) {
-  print('Hello world: ${oppps_exaam.calculate()}!');
+void printsquares(int i) {
+  for (int i = 1; i <= 10; i++);
+  print(i * i);
+}
+
+void main() {
+  printsquares(5);
+  separate();
+}
+
+void separate() {
+  print('1,4,9,16,25,');
 }
---
+++ bin/oppps_examq2.dart
@@ -0,0 +1,26 @@
+abstract class animal {
+  void makesound() {
+    print("makesound");
+  }
+}
+
+class dog extends animal {
+  @override
+  void makesound() {
+    print("woof");
+  }
+}
+
+class cat extends animal {
+  @override
+  void makesound() {
+    print("moew");
+  }
+}
+
+void main() {
+  dog d1 = new dog();
+  d1.makesound();
+  cat c1 = new cat();
+  c1.makesound();
+}
